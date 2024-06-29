e430e4ae4c8d6d33979d4a4ad971c77d893a5e90
kamal-ayman <kamal442834@fci.bu.edu.eg>
2024-06-30 01:14:13 +0300

    update gradle

--- lib/captain_eco.dart
+++ lib/captain_eco.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CaptainEco extends StatelessWidget {
   const CaptainEco({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Firebase Example App'),
